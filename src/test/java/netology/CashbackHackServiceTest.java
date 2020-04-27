01102832a9ed9aa6fec8d839c814f1b095241b7e
melnioks <ommelnikova@gmail.com>
2020-04-27 12:57:55 +0200

    Add JUnit dependency for Parameterized Tests Add .csv sources for cashback tests Replace chachback single tetst by parameterized tests

--- src/test/java/netology/CashbackHackServiceTest.java
+++ src/test/java/netology/CashbackHackServiceTest.java
@@ -2,7 +2,9 @@ package netology;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -15,41 +17,20 @@ class CashbackHackServiceTest {
         cashbackHackService = new CashbackHackService();
     }
 
-    @Test
-    @DisplayName("Ammount in the cart is 999")
-    void shouldReturn1For999() {
-        int actual = cashbackHackService.remain(999);
-        assertEquals(actual, 1);
+    @ParameterizedTest
+    @CsvFileSource(resources = "/cashbackTestDataPositive.csv", numLinesToSkip = 1)
+    @DisplayName("Amount in the cart is more than 0")
+    void shouldReturnAmountToNext1000(int amount, int expected) {
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(actual, expected);
     }
 
-    @Test
-    @DisplayName("Ammount in the cart is 1001")
-    void shouldReturn999For1001() {
-        int actual = cashbackHackService.remain(1001);
-        assertEquals(actual, 999);
-    }
-
-    @Test
-    @DisplayName("Ammount in the cart is 1000")
-    void shouldReturn0For1000() {
-        int actual = cashbackHackService.remain(1000);
-        assertEquals(actual, 0);
-    }
-
-    @Test
-    @DisplayName("Ammount in the cart is 0")
-    void shouldReturnErrorMessageFor0() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/cashbackTestDataException.csv", numLinesToSkip = 1)
+    @DisplayName("Amount in the cart is 0 or less")
+    void shouldReturnExceptionForIllegalAmount(int amount, String message) {
             Exception exception = assertThrows(IllegalArgumentException.class, () ->
-                    cashbackHackService.remain(0));
-            assertEquals("amount must be greater than zero", exception.getMessage());
+                    cashbackHackService.remain(amount));
+            assertEquals(message, exception.getMessage());
     }
-
-    @Test
-    @DisplayName("Ammount in the cart is -1")
-    void shouldReturnErrorMessageForLess0() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () ->
-                cashbackHackService.remain(-1));
-        assertEquals("amount must be greater than zero", exception.getMessage());
-    }
-
 }
