9b108d10bd348056da6a3c832099372cd89af8cc
melnioks <ommelnikova@gmail.com>
2020-04-30 20:45:24 +0200

    Move instantiation to tests Add a failure message for positive test cases

--- src/test/java/netology/CashbackHackServiceTest.java
+++ src/test/java/netology/CashbackHackServiceTest.java
@@ -1,24 +1,21 @@
 package netology;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CashbackHackServiceTest {
-
-    private CashbackHackService cashbackHackService = new CashbackHackService();
 
     @ParameterizedTest
     @CsvFileSource(resources = "/cashbackTestDataPositive.csv", numLinesToSkip = 1)
     @DisplayName("Amount in the cart is more than 0")
-    void shouldReturnAmountToNext1000(int amount, int expected) {
+    void shouldReturnAmountToNext1000(int amount, int expected, String failureMessage) {
+        CashbackHackService cashbackHackService = new CashbackHackService();
         int actual = cashbackHackService.remain(amount);
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, failureMessage);
     }
 
 
@@ -26,7 +23,8 @@ class CashbackHackServiceTest {
     @CsvFileSource(resources = "/cashbackTestDataException.csv", numLinesToSkip = 1)
     @DisplayName("Amount in the cart is 0 or less")
     void shouldReturnExceptionForIllegalAmount(int amount, String message) {
-            Exception exception = assertThrows(IllegalArgumentException.class, () ->
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
                     cashbackHackService.remain(amount));
             assertEquals(message, exception.getMessage());
     }
